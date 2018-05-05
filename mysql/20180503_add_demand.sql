ef72000cafee90f814c040cafc77134fa27e4b69
Abner Du <abner.du@neulion.com.cn>
2018-05-05 23:34:54 +0800

    add demand

---
+++ mysql/20180503_add_demand.sql
@@ -0,0 +1,46 @@
+CREATE TABLE STUDENT_ORDER (
+	STUDENT_ORGER_ID int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	STUDENT_ID int NOT null,
+	DEMAND varchar(200) NULL,
+	REQUIREMENT varchar(2000) NULL,
+	STATUS varchar(30) NULL,
+	UPDATE_DATE datetime NULL
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+insert into STUDENT_ORDER (STUDENT_ID, REQUIREMENT, STATUS, UPDATE_DATE) select STUDENT_ID, REQUIREMENT, STATUS, UPDATE_DATE from STUDENT; 
+
+CREATE TABLE TEACHER_ORDER (
+	TEACHER_ORGER_ID int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	TEACHER_ID int NOT null,
+	DEMAND varchar(200) NULL,
+	REQUIREMENT varchar(2000) NULL,
+	STATUS varchar(30) NULL,
+	UPDATE_DATE datetime NULL
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+insert into TEACHER_ORDER (TEACHER_ID, REQUIREMENT, STATUS, UPDATE_DATE) select TEACHER_ID, REQUIREMENT, 'NOT_FINISH' as STATUS, UPDATE_DATE from TEACHER;
+
+CREATE TABLE DEMAND (
+	DEMAND_ID int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	NAME varchar(200) NULL,
+	SCOPE varchar(30) NULL,
+	UPDATE_DATE datetime NULL
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+insert into DEMAND (NAME, SCOPE, UPDATE_DATE) values ('需要专业课辅导','student',localtime());
+insert into DEMAND (NAME, SCOPE, UPDATE_DATE) values ('需要专业课资料+答疑','student',localtime());
+insert into DEMAND (NAME, SCOPE, UPDATE_DATE) values ('需要专业课资料','student',localtime());
+insert into DEMAND (NAME, SCOPE, UPDATE_DATE) values ('其他','student',localtime());
+
+insert into DEMAND (NAME, SCOPE, UPDATE_DATE) values ('提供专业课辅导','teacher',localtime());
+insert into DEMAND (NAME, SCOPE, UPDATE_DATE) values ('提供专业课资料+答疑','teacher',localtime());
+insert into DEMAND (NAME, SCOPE, UPDATE_DATE) values ('提供专业课资料','teacher',localtime());
+insert into DEMAND (NAME, SCOPE, UPDATE_DATE) values ('其他','teacher',localtime());
+
+
+ALTER TABLE STUDENT DROP COLUMN REQUIREMENT;
+ALTER TABLE STUDENT DROP COLUMN STATUS;
+ALTER TABLE TEACHER DROP COLUMN REQUIREMENT;
+
+insert into CONFIG (NAME, INPUT, DESCRIPTION, UPDATE_DATE) 
+values ('STUDENT_EMAIL','true','whether send the student email',localtime());
