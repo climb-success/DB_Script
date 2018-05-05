98f577364c021be3cef3676cb463348faadad072
Abner Du <abner.du@neulion.com.cn>
2018-05-06 02:27:37 +0800

    add out feed

---
+++ mysql/20180506_add_out_feed.sql
@@ -0,0 +1,2 @@
+insert into CONFIG (NAME, INPUT, DESCRIPTION, UPDATE_DATE) 
+values ('TEACHER_STUDENT_OUT_XLS_PATH','C://apache-tomcat-8.0.47//webapps//storage//outdate','out feed path',localtime());
